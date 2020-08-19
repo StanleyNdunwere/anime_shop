509653708f4b791f20028c6b617a3f119ca3d653
Stanley Ndunwere <StanleyNdunwere@gmail.com>
2020-08-19 07:49:25 +0100

    abstracted the details of the components to different objects

--- src/components/menu_items/menu_item_container.component.jsx
+++ src/components/menu_items/menu_item_container.component.jsx
@@ -10,20 +10,55 @@ class MenuItemContainer extends Component {
 
   constructor(props) {
     super(props)
+    this.menuItems = [
+      {
+        title: "ADVENTURE",
+        src: adventure,
+        id: 1,
+      },
+      {
+        title: "ACTION",
+        src: action,
+        id: 2,
+      },
+      {
+        title: "MYSTERY",
+        src: mystery,
+        id: 3,
+      }
+    ]
+
+    this.menuTypes = [
+      {
+        title: "Manga",
+        src: manga,
+        id: 4,
+      },
+      {
+        title: "TV",
+        src: tv,
+        id: 5,
+      },
+    ]
   }
 
 
   render() {
     return (
-      <div className= "all-menu-container">
+      <div className="all-menu-container">
         <div className="menu-items-container">
-          <MenuItem src={adventure} title={"ADVENTURE"} ></MenuItem>
-          <MenuItem src={mystery} title={"MYSTERY"}></MenuItem>
-          <MenuItem src={action} title={"ACTION"}></MenuItem>
+          {this.menuItems.map((menuItem) => {
+            return <MenuItem src={menuItem.src}
+              title={menuItem.title}
+              key={menuItem.id}></MenuItem>
+          })}
         </div>
         <div className="anime-type-contaner">
-          <MenuItem src={manga} title={"MANGA"}></MenuItem>
-          <MenuItem src={tv} title={"TV"}></MenuItem>
+          {this.menuTypes.map((menuType) => {
+            return <MenuItem src={menuType.src}
+              title={menuType.title}
+              key={menuType.id}></MenuItem>
+          })}
         </div>
       </div>
 
