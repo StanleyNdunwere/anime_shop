7f0f0e53deb24b471997bbca42ef9cef92a1dc0b
Stanley Ndunwere <StanleyNdunwere@gmail.com>
2020-08-22 13:34:42 +0100

    data format modified

--- src/components/anime_category_page/anime_category_page.component.jsx
+++ src/components/anime_category_page/anime_category_page.component.jsx
@@ -1,14 +1,30 @@
 import React, { Component } from 'react'
 import "../anime_category_page/anime-category-page.style.css"
 import SingleCategory from "./single_category.component"
+import animeData from "../../base/data"
 
 
 class AnimeCategoryPage extends Component {
-
   constructor(props) {
     super(props)
     this.componentName = "anime category page"
+    this.GENRE = "genre"
+    this.MEDIA = "media"
+    this.state = {
+      mediaCategory: [],
+      genreCategory: [],
+
+    }
   }
+
+  componentDidMount() {
+    let segmentedDataMedia = this.categoriseAnime(animeData, this.MEDIA)
+    let segmentedDataGenre = this.categoriseAnime(animeData, this.GENRE)
+    // console.log(segmentedDataGenre)
+
+
+  }
+
 
   render() {
     return (
@@ -29,5 +45,49 @@ class AnimeCategoryPage extends Component {
     )
   }
 
+
+
+  categoriseAnime = (animeData, genre = this.GENRE) => {
+    let categories = this.getGenresOrMedia(animeData, genre = this.GENRE)
+    let segmentedData = []
+    categories.forEach(category => {
+      let groupedData = {};
+      groupedData = {
+        group: category,
+        groupedData: []
+      }
+      animeData.forEach((anime) => {
+        if (genre === this.GENRE) {
+          if (anime.genre === category) {
+            groupedData.groupedData.push(anime)
+          }
+        } else {
+          if (anime.media === category) {
+            groupedData.groupedData.push(anime)
+          }
+        }
+      })
+      segmentedData.push(groupedData)
+    });
+    return segmentedData
+  }
+
+  getGenresOrMedia = (animeData, genre = this.GENRE) => {
+    let categories = []
+    switch (genre) {
+      case this.GENRE:
+        categories = animeData.map((anime) => {
+          return anime.genre
+        })
+        break
+      case this.MEDIA:
+      default:
+        categories = animeData.map((anime) => {
+          return anime.media
+        })
+        break
+    }
+    return [...new Set(categories)]//remove duplicates
+  }
 }
 export default AnimeCategoryPage;
